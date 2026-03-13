9be7a5a34304842c4dd4b3a6cf1eae60251cefe1
israelpinto <hartl123452019@gmail.com>
2026-03-13 14:01:21 +0200

    fix format

--- components/admin/AdminHeader.tsx
+++ components/admin/AdminHeader.tsx
@@ -25,8 +25,8 @@ export function AdminHeader() {
         <div className="flex items-center space-x-4 space-x-reverse">
           {/* התראות */}
           <button className="relative p-2 text-gray-400 hover:text-white transition-colors">
-               <Bell className="w-5 h-5" />
-                <span className="absolute top-0 left-0 w-2 h-2 bg-red-500 rounded-full"></span>
+            <Bell className="w-5 h-5" />
+            <span className="absolute top-0 left-0 w-2 h-2 bg-red-500 rounded-full"></span>
           </button>
 
           {/* פרופיל */}
